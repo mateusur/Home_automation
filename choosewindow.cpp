758b3ce63ad619b635abe2a14e128f9a13a7ec4b
Mateusz Urbaniak <241614@student.pwr.edu.pl>
2020-08-14 16:23:42 +0200

    Topic subscription and icons added

--- choosewindow.cpp
+++ choosewindow.cpp
@@ -11,13 +11,24 @@ ChooseWindow::ChooseWindow(QWidget *parent)
     set_icons();
 
     //MQTT
-    connect(chickencoop_window,SIGNAL(publish_msg(QString, QByteArray)),this,SLOT(publish_msg(QString, QByteArray)));
+    connect(chickencoop_window,SIGNAL(publish_message(QString, QByteArray)),this,SLOT(publish_message(QString, QByteArray))); // Send "UP", "DOWN" from chickencoop window
     m_client = new QMqttClient(this);
-    //m_client->setHostname("192.168.1.8");
-    //m_client->setPort(1883);
-    //m_client->setClientId("Qt_app");
-    //m_client->connectToHost();
+    m_client->setHostname("192.168.1.8");
+    m_client->setPort(1883);
+    m_client->setClientId("Qt_app");
+    m_client->connectToHost();
 
+    connect(m_client,SIGNAL(messageReceived( QByteArray,  QMqttTopicName)),this,SLOT(message_handler(QByteArray,  QMqttTopicName)));
+    connect(m_client, &QMqttClient::messageReceived, this, [this](const QByteArray &message, const QMqttTopicName &topic) {
+           const QString content = QDateTime::currentDateTime().toString()
+                       + QLatin1String(" Received Topic: ")
+                       + topic.name()
+                       + QLatin1String(" Message: ")
+                       + message
+                       + QLatin1Char('\n');
+           ui->editLog->insertPlainText(content);
+       });
+    QTimer::singleShot(5000, this, &ChooseWindow::set_subscription);
 
 }
 
@@ -32,7 +43,7 @@ void ChooseWindow::show_window()
     this->show();
 }
 
-void ChooseWindow::publish_msg(QString topic, QByteArray msg)
+void ChooseWindow::publish_message(QString topic, QByteArray msg)
 {
     qDebug() << topic;
     qDebug() << msg;
@@ -46,6 +57,37 @@ void ChooseWindow::on_chickencoop_button_clicked()
     this->hide();
     qDebug() << "Hello";
 
+}
+
+void ChooseWindow::message_handler(QByteArray message,  QMqttTopicName topic)
+{
+    const QMqttTopicName temp("chickencoop/temperature");
+    const QMqttTopicName hum("chickencoop/humidity");
+    const QMqttTopicName doors("chickencoop/doors/status");
+    const QString doors_opened("opened");
+    const QString doors_closed("closed");
+    int w = ui->label_hum->width();
+    int h = ui->label_hum->height();
+    const QPixmap pixmap_doors_opened(":/Icons/open_door_96.png");
+    const QPixmap pixmap_doors_closed(":/Icons/door_closed_96.png");
+    ui->label_doors->setPixmap(pixmap_doors_opened.scaled(w,h,Qt::KeepAspectRatioByExpanding));
+
+   qDebug() <<"Odebralem info";
+   qDebug() << message;
+   qDebug() << topic;
+   if(topic == temp)
+       ui->lineEdit_temp->setText(message);
+   else if (topic == hum) {
+       ui->lineEdit_hum->setText(message);
+   }
+   else if (topic == doors) {
+       if(message == doors_opened){
+            ui->label_doors->setPixmap(pixmap_doors_opened.scaled(w,h,Qt::KeepAspectRatioByExpanding));
+       }
+       else if(message == doors_closed){
+            ui->label_doors->setPixmap(pixmap_doors_closed.scaled(w,h,Qt::KeepAspectRatioByExpanding));
+       }
+   }
 }
 
 void ChooseWindow::set_icons()
@@ -69,4 +111,30 @@ void ChooseWindow::set_icons()
     QIcon ButtonIcon_lights(pixmap_lights);
     ui->lights_button->setIcon(ButtonIcon_lights);
     ui->lights_button->setIconSize(pixmap_lights.rect().size());
+
+    int w = ui->label_hum->width();
+    int h = ui->label_hum->height();
+    QPixmap pixmap_thermometer(":/Icons/thermometer_64.png");
+    QPixmap pixmap_humidity(":/Icons/humidity_80.png");
+    ui->label_temp->setPixmap(pixmap_thermometer.scaled(w,h,Qt::KeepAspectRatioByExpanding));
+    ui->label_hum->setPixmap(pixmap_humidity.scaled(w,h,Qt::KeepAspectRatioByExpanding));
+    ui->label_temp_2->setText(" °C");
+    ui->label_hum_2->setText(" %");
+}
+
+void ChooseWindow::set_subscription()
+{
+    const QString temp("chickencoop/temperature");
+    const QString hum("chickencoop/humidity");
+    const QString doors("chickencoop/doors/status");
+    m_client->subscribe(temp);
+    m_client->subscribe(hum);
+    m_client->subscribe(doors);
+
+}
+
+void ChooseWindow::on_buttonSubscribe_clicked()
+{
+    auto subscription = m_client->subscribe(ui->lineEditTopic->text());
+    qDebug() << subscription->state();
 }
--- choosewindow.h
+++ choosewindow.h
@@ -9,6 +9,10 @@
 #include "chickencoop.h"
 
 #include <QMqttClient>
+#include <QMqttSubscription>
+#include <QThread>
+#include <QTimer>
+
 #include <QString>
 //::fromlatin1
 
@@ -26,13 +30,18 @@ public:
 
 private slots:
     void show_window();
-    void publish_msg(QString topic, QByteArray msg);
+    void publish_message(QString topic, QByteArray msg);
     void on_chickencoop_button_clicked();
+    void set_subscription();
+    void message_handler(QByteArray message,  QMqttTopicName topic);
+    void on_buttonSubscribe_clicked();
 
 private:
     Ui::ChooseWindow *ui;
     Chickencoop *chickencoop_window;
     QMqttClient *m_client;
+
     void set_icons();
+
 };
 #endif // CHOOSEWINDOW_H
