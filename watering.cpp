9cf6a3f68ee61c63bf237bbd9577cd81be502f54
Mateusz Urbaniak <241614@student.pwr.edu.pl>
2020-08-26 15:18:06 +0200

    Connecting with Droplet class and choosewindow

--- watering.cpp
+++ watering.cpp
@@ -6,9 +6,26 @@ Watering::Watering(QWidget *parent) :
     ui(new Ui::Watering)
 {
     ui->setupUi(this);
+
+    for(auto i=0; i<drop_count; i++){
+        labels[i] = new QLabel(this);
+    }
+    for(auto i=0; i<drop_count; i++){
+        droplets[i] = new Droplet(labels[i]);
+    }
 }
 
 Watering::~Watering()
 {
+    qDebug() << "Destruktor garden window";
     delete ui;
+    for(auto i=0; i<drop_count; i++){
+        delete droplets[i];
+    }
+}
+
+void Watering::on_return_button_clicked()
+{
+    this->hide();
+      emit change_window();
 }
--- watering.h
+++ watering.h
@@ -2,7 +2,8 @@
 #define WATERING_H
 
 #include <QDialog>
-
+#include "droplet.h"
+#include <QDebug>
 namespace Ui {
 class Watering;
 }
@@ -15,8 +16,16 @@ public:
     explicit Watering(QWidget *parent = nullptr);
     ~Watering();
 
+private slots:
+    void on_return_button_clicked();
+signals:
+    void change_window();
+
 private:
     Ui::Watering *ui;
+    static const int drop_count = 15;
+    QLabel *labels[drop_count];
+    Droplet *droplets[drop_count];
 };
 
 #endif // WATERING_H
