1c86aa75adc374908d98a8f645f53267aa89391b
Mateusz Urbaniak <urban1998@wp.pl>
2020-08-27 16:52:19 +0200

    Publishing time with mqtt added

--- watering.cpp
+++ watering.cpp
@@ -13,6 +13,7 @@ Watering::Watering(QWidget *parent) :
     for(auto i=0; i<drop_count; i++){
         droplets[i] = new Droplet(labels[i]);
     }
+
 }
 
 Watering::~Watering()
@@ -28,4 +29,43 @@ void Watering::on_return_button_clicked()
 {
     this->hide();
       emit change_window();
+}
+
+void Watering::on_radioButton_once_clicked()
+{
+    if(ui->radioButton_once->isChecked()){
+        ui->checkBox_monday->setEnabled(false);
+        ui->checkBox_tuesday->setEnabled(false);
+        ui->checkBox_wednesday->setEnabled(false);
+        ui->checkBox_thursday->setEnabled(false);
+        ui->checkBox_friday->setEnabled(false);
+        ui->checkBox_saturday->setEnabled(false);
+        ui->checkBox_sunday->setEnabled(false);
+    }
+    else{
+        ui->checkBox_monday->setEnabled(true);
+        ui->checkBox_tuesday->setEnabled(true);
+        ui->checkBox_wednesday->setEnabled(true);
+        ui->checkBox_thursday->setEnabled(true);
+        ui->checkBox_friday->setEnabled(true);
+        ui->checkBox_saturday->setEnabled(true);
+        ui->checkBox_sunday->setEnabled(true);
+    }
+
+}
+
+void Watering::on_pushButton_accept_clicked()
+{
+    QMessageBox msgBox(this);
+    msgBox.setText("Musisz zaznaczyc regularność nawadniania.");
+
+    if(ui->radioButton_once->isChecked() || ui->checkBox_monday->isChecked() || ui->checkBox_tuesday->isChecked() || ui->checkBox_wednesday->isChecked()
+            || ui->checkBox_thursday->isChecked() || ui->checkBox_friday->isChecked() || ui->checkBox_saturday->isChecked() || ui->checkBox_sunday->isChecked()){
+    qDebug() << ui->doubleSpinBox_interval->value();
+    double minutes = ui->doubleSpinBox_interval->value();
+    QByteArray array(reinterpret_cast<const char*>(&minutes), sizeof(minutes));
+    emit publish_msg(topic,array);
+
+    }else
+        msgBox.exec();
 }
--- watering.h
+++ watering.h
@@ -4,6 +4,8 @@
 #include <QDialog>
 #include "droplet.h"
 #include <QDebug>
+#include <QMessageBox>
+
 namespace Ui {
 class Watering;
 }
@@ -18,14 +20,20 @@ public:
 
 private slots:
     void on_return_button_clicked();
+    void on_radioButton_once_clicked();
+
+    void on_pushButton_accept_clicked();
+
 signals:
     void change_window();
+    void publish_msg(const QString &topic,const QByteArray &msg);
 
 private:
     Ui::Watering *ui;
     static const int drop_count = 15;
     QLabel *labels[drop_count];
     Droplet *droplets[drop_count];
+    const QString topic = "garden/watering/solenoid";
 };
 
 #endif // WATERING_H
