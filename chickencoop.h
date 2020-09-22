a08ee3dd3962d5c7a795aab40cb4e80366d115e9
Mateusz Urbaniak <urban1998@wp.pl>
2020-09-22 15:11:29 +0200

    Doxygen comments added

--- chickencoop.h
+++ chickencoop.h
@@ -19,26 +19,67 @@ class Chickencoop : public QDialog
     Q_OBJECT
 
 public:
+    /*!
+    * \brief Konstruktor klasy Chickencoop.
+    * \param[in] parent
+    */
     explicit Chickencoop(QWidget *parent = nullptr);
     ~Chickencoop();
 
 private slots:
+    /*!
+    * \brief Metoda odpowiedzialna za powrót do okna głównego.
+    */
     void on_return_button_clicked();
+    /*!
+    * \brief Metoda odpowiedzialna za podniesienie drzwi kurnika.
+    */
     void on_up_button_clicked();
+    /*!
+    * \brief Metoda odpowiedzialna za opuszczenie drzwi kurnika.
+    */
     void on_down_button_clicked();
-
+    /*!
+    * \brief Metoda odpowiedzialna za zatwierdzenie zmiany ip oraz portu z którego jest nadawny stream.
+    */
     void on_pushButton_stream_clicked();
 
 signals:
+    /*!
+    * \brief Sygnał odpowiedzialny za przełączanie między oknem głównym a oknem Chickencoop.
+    */
     void change_window();
+    /*!
+    * \brief Sygnał odpowiedzialny za wysyłanie wiadomości przez MQTT.
+    * \param[in] pub_topic - temat do, którego publikujemy.
+    * \param[in] msg - wiadomość jaką publikujemy.
+    */
     void publish_msg(const QString &pub_topic,const QByteArray &msg);
 private:
     Ui::Chickencoop *ui;
+    /*!
+    * \brief Metoda ustawiająca ikony strzałek na poszczególne przyciski.
+    */
     void set_icons();
+    /*!
+    * \brief Temat do którego są publikowane wiadomości MQTT.
+    */
     const QString pub_topic = "chickencoop/door";
+    /*!
+    * \brief Wskaźnik na player do streamu.
+    */
     QMediaPlayer *player;
+    /*!
+    * \brief Wskaźnik na Video Widget potrzebny do odtworzenia streamu.
+    */
     QVideoWidget *videoWidget;
+    /*!
+    * \brief Wskaźnik na ustawienia z których pobierane jest ip oraz port stremu.
+    */
     QSettings *settings;
+    /*!
+    * \brief Link do streamu.
+    */
     QUrl link;
 
 };
--- choosewindow.h
+++ choosewindow.h
@@ -29,38 +29,111 @@ class ChooseWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    /*!
+    * \brief Konstruktor klasy ChooseWindow.
+    * \param[in] parent
+    */
     ChooseWindow(QWidget *parent = nullptr);
     ~ChooseWindow();
 public slots:
+    /*!
+    * \brief Metoda odpowiedzialna za przesłanie wiadmości za pomocą MQTT.
+    * \param[in] topic - temat do, którego publikujemy.
+    * \param[in] msg - wiadomość jaką publikujemy.
+    */
     void publish_message(const QString &topic,const QByteArray &msg);
+    /*!
+    * \brief Metoda odpowiedzialna za przesłanie wiadmości za pomocą MQTT z możliwością ustawienia przetrzymania wiadomości.
+    * \param[in] topic - temat do, którego publikujemy.
+    * \param[in] msg - wiadomość jaką publikujemy.
+    * \param[in] qos - jakość serwisu (quality of service).
+    * \param[in] retain - czy wiadmość ma być przetrzymana przez server (broker).
+    */
     void publish_message_retain(const QString &topic,const QByteArray &msg,quint8 qos = 0,bool retain = false);
 private slots:
+    /*!
+    * \brief Metoda otwierająca okno kurnika.
+    */
     void on_chickencoop_button_clicked();
+    /*!
+    * \brief Metoda otwierająca okno pogody.
+    */
     void on_weather_button_clicked();
+    /*!
+    * \brief Metoda otwierająca okno nawadniania.
+    */
     void on_water_button_clicked();
+    /*!
+    * \brief Metoda otwierająca główne okno.
+    */
     void show_window();
+    /*!
+    * \brief Metoda ustawiająca subskrypcje dla wszytkich klas.
+    */
     void set_subscription();
+    /*!
+    * \brief Metoda obsługująca odebrane wiadomości.
+    */
     void message_handler(QByteArray message,  QMqttTopicName topic);
+    /*!
+    * \brief Metoda otwierająca okno z opcjami MQTT.
+    */
     void on_action_optionsMQTT_2_triggered();
+    /*!
+    * \brief Metoda otwierająca okno z opcjami pogody.
+    */
     void on_action_options_weather_triggered();
+    /*!
+    * \brief Metoda otwierająca okno z informacjami o autorze i licencji ikon.
+    */
     void on_action_about_author_triggered();
+    /*!
+    * \brief Metoda ustawiająca opcje związane z klientem MQTT.
+    */
     void set_Mqtt();
 signals:
 
 private:
     Ui::ChooseWindow *ui;
+    /*!
+    * \brief Wskaźnik na okno kurnika.
+    */
     Chickencoop *chickencoop_window;
+    /*!
+    * \brief Wskaźnik na okno pogody.
+    */
     Weather *weather_window;
+    /*!
+    * \brief Wskaźnik na okno nawadniania.
+    */
     Watering *watering_window;
+    /*!
+    * \brief Wskaźnik na okno opcji MQTT.
+    */
     SettingsMQTT *mqtt_settings_window;
+    /*!
+    * \brief Wskaźnik na okno opcji pogody.
+    */
     SettingsWeather *weather_settings_window;
+    /*!
+    * \brief Wskaźnik na okno informacji o autorze.
+    */
     Author *about_author_window;
+    /*!
+    * \brief Wskaźnik na klienta MQTT.
+    */
     QMqttClient *m_client;
-//    QString server_ip;
-//    int port;
+    /*!
+    * \brief Metoda ustawiająca wszystkie ikony.
+    */
     void set_icons();
-
+    /*!
+    * \brief Tablica przechowująca subskrybowane tematy związane z kurnikiem.
+    */
     const std::array<QString,3> sub_topics_chickencoop = {"chickencoop/temperature","chickencoop/humidity","chickencoop/doors/status"};
+    /*!
+    * \brief Tablica przechowująca subskrybowane tematy związane z nawadnianiem.
+    */
     const std::array<QString,7> sub_topics_watering = {"garden/watering/solenoid/sunday", "garden/watering/solenoid/monday", "garden/watering/solenoid/tuesday", "garden/watering/solenoid/wednesday"
                                                         , "garden/watering/solenoid/thursday", "garden/watering/solenoid/friday", "garden/watering/solenoid/saturday"};
 };
