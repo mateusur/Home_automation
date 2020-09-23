505b41150c90eca8df71259c774cdf43d3af6541
Mateusz Urbaniak <urban1998@wp.pl>
2020-09-23 11:09:12 +0200

    Doxygen comments added

--- watering.h
+++ watering.h
@@ -18,31 +18,94 @@ class Watering : public QDialog
     Q_OBJECT
 
 public:
+    /*!
+    * \brief Konstruktor klasy Watering.
+    * \param[in] parent
+    */
     explicit Watering(QWidget *parent = nullptr);
     ~Watering();
 public slots:
+    /*!
+    * \brief Metoda odpowiedzialna za obsługę odebranych wiadomości.
+    * \param[in] message -  wiadomość jaka została opublikowana.
+    * \param[in] topic - temat do, którego wiadomość została opublikowana.
+    */
     void message_handler(QByteArray message,  QMqttTopicName topic);
 private slots:
+    /*!
+    * \brief Metoda odpowiedzialna za powrót do głównego okna.
+    */
     void on_return_button_clicked();
+    /*!
+    * \brief Metoda odpowiedzialna za jednorazowe nawodnienie.
+    */
     void on_radioButton_once_clicked();
+    /*!
+    * \brief Metoda odpowiedzialna za opublikowanie wiadomości.
+    */
     void on_pushButton_accept_clicked();
+    /*!
+    * \brief Metoda odpowiedzialna za odświeżenie timera.
+    */
     void updateCountdown();
+    /*!
+    * \brief Metoda odpowiedzialna wyłączenie nawadniania.
+    */
     void on_pushButton_turn_off_clicked();
 
 signals:
+    /*!
+    * \brief Sygnał odpowiedzialny za zmianę okna.
+    */
     void change_window();
+    /*!
+    * \brief Sygnał odpowiedzialny za wysłanie wiadomości przez MQTT.
+    * \param[in] topic - temat do, którego publikujemy.
+    * \param[in] msg - wiadomość jaką publikujemy.
+    */
     void publish_msg(const QString &topic,const QByteArray &msg);
+    /*!
+    * \brief Sygnał odpowiedzialny za wysłanie wiadomości przez MQTT z opcją przetrzymania wiadomości.
+    * \param[in] topic - temat do, którego publikujemy.
+    * \param[in] msg - wiadomość jaką publikujemy.
+    * \param[in] qos - jakość serwisu (quality of service).
+    * \param[in] retain - czy wiadmość ma być przetrzymana przez server (broker).
+    */
     void publish_message_retain(const QString &topic,const QByteArray &msg,quint8 qos = 0,bool retain = false);
 private:
     Ui::Watering *ui;
+    /*!
+    * \brief Liczba kropel, które chcemy animować.
+    */
     static const int drop_count = 20;
+    /*!
+    * \brief Tablica etykiet do animacji kropel.
+    */
     QLabel *labels[drop_count];
+    /*!
+    * \brief Tablica kropli.
+    */
     Droplet *droplets[drop_count];
+    /*!
+    * \brief Tablica tematów do których jest wysyłane są wiadomości.
+    */
     const QString topics_watering[8] = { "garden/watering/solenoid/sunday", "garden/watering/solenoid/monday", "garden/watering/solenoid/tuesday", "garden/watering/solenoid/wednesday"
                                 , "garden/watering/solenoid/thursday", "garden/watering/solenoid/friday", "garden/watering/solenoid/saturday", "garden/watering/solenoid" };
+    /*!
+    * \brief Timer odświeżający minutnik.
+    */
     QTimer *timer;
-    QTimer *timer2;
+    /*!
+    * \brief Timer do odświeżający minutnik ochładzania.
+    */
+    QTimer *timer2; // Sa dwa poniewaz musze wiedziec, ktory wyslal informacje (slot itd)
+    /*!
+    * \brief Minutnik do wyświetlania pozostałego czasu nawadniania/ochładzania.
+    */
     QTime countdown;
+    /*!
+    * \brief Czas schładzania elektrozaworu.
+    */
     const unsigned short cooldown_time = 15;
 
 };
--- weather.h
+++ weather.h
@@ -21,40 +21,134 @@ class Weather : public QDialog
     Q_OBJECT
 
 public:
+    /*!
+    * \brief Konstruktor klasy Weather.
+    * \param[in] parent
+    */
     explicit Weather(QWidget *parent = nullptr);
     ~Weather();
 public slots:
+    /*!
+    * \brief Metoda pobierająca dane pogodowe z serwera.
+    */
     void on_data_changed();
 private slots:
+    /*!
+    * \brief Metoda przewarzająca otrzymane informacje.
+    * \param[in] reply - wskaźnik na odpowiedź serwera.
+    */
     void managerFinished(QNetworkReply *reply);
+    /*!
+    * \brief Metoda powodująca powrót do głównego okna.
+    */
     void on_return_button_clicked();
+    /*!
+    * \brief Metoda odświeżająca pogodę.
+    */
     void on_pushButton_refresh_clicked();
 
 private:
     Ui::Weather *ui;
+    /*!
+    * \brief Manager dostępu do sieci.
+    */
     QNetworkAccessManager *manager;
+    /*!
+    * \brief Żądanie potrzebne managerowi.
+    */
     QNetworkRequest request;
+    /*!
+    * \brief Tablica odczuwalnych temperatur.
+    */
     QVector<double> v_feels_like;
+    /*!
+    * \brief Tablica opadów deszczu.
+    */
     QVector<double> v_rain;
+    /*!
+    * \brief Tablica ciścień.
+    */
     QVector<int> v_pressure;
+    /*!
+    * \brief Tablica temperatur.
+    */
     QVector<double> v_temp;
+    /*!
+    * \brief Tablica opisów.
+    */
     QVector<QString> v_description;
+    /*!
+    * \brief Tablica ikon.
+    */
     QVector<QString> v_icon;
+    /*!
+    * \brief Tablica dat.
+    */
     QVector<QString> v_time;
+    /*!
+    * \brief Tablica chmur.
+    */
     QVector<int> v_clouds;
+    /*!
+    * \brief Metoda ustawiająca ikone na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] icon - nazwa ikony.
+    */
     void set_icon(QLabel *label, QString icon);
+    /*!
+    * \brief Metoda ustawiająca temperature na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] temp - temperatura.
+    */
     void set_temp(QLabel *label, double temp);
+    /*!
+    * \brief Metoda ustawiająca temperature odczuwalną na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] feels_like - temperatura odczuwalna.
+    */
     void set_feels(QLabel *label, double feels_like);
+    /*!
+    * \brief Metoda ustawiająca opady na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] rain - wartość opadów.
+    */
     void set_rain(QLabel *label, double rain);
+    /*!
+    * \brief Metoda ustawiająca zachmurzenie na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] cloudines - wartość zachmurzenia.
+    */
     void set_clouds(QLabel *label,int cloudines);
+    /*!
+    * \brief Metoda ustawiająca zachmurzenie na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] pressure - wartość ciśnienia.
+    */
     void set_pressure(QLabel *label,int pressure);
+    /*!
+    * \brief Metoda ustawiająca opis pogody na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] description - opis pogody.
+    */
     void set_description(QLabel *label, QString description);
+    /*!
+    * \brief Metoda ustawiająca datę na etykietę.
+    * \param[in] label - etykieta.
+    * \param[in] time - data.
+    */
     void set_time(QLabel *label, QString time);
+    /*!
+    * \brief Metoda ustawiająca opisy i ikony.
+    */
     void set_all_icons();
 
 
 signals:
+    /*!
+    * \brief Sygnał .
+    */
     void daneZmienione(QJsonDocument dane);
+
     void change_window();
 };
 
