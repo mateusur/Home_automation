78bc6937105c0d74dad6154d49dd3257d7cd1c29
Mateusz Urbaniak <urban1998@wp.pl>
2020-10-05 15:48:12 +0200

    Long Doxygen comments added

--- watering.h
+++ watering.h
@@ -20,6 +20,8 @@ class Watering : public QDialog
 public:
     /*!
     * \brief Konstruktor klasy Watering.
+    *
+    * Konstruktor klasy Watering.
     * \param[in] parent
     */
     explicit Watering(QWidget *parent = nullptr);
@@ -27,6 +29,9 @@ public:
 public slots:
     /*!
     * \brief Metoda odpowiedzialna za obsługę odebranych wiadomości.
+    *
+    * Metoda odpowiedzialna za obsługę odebranych wiadomości - ustawia etykiete odpowiedniego dnia na godzine i czas nawadniania.
+    * Brak etykiety danego dnia oznacza brak nawadniania tego dnia.
     * \param[in] message -  wiadomość jaka została opublikowana.
     * \param[in] topic - temat do, którego wiadomość została opublikowana.
     */
@@ -34,38 +39,56 @@ public slots:
 private slots:
     /*!
     * \brief Metoda odpowiedzialna za powrót do głównego okna.
+    *
+    * Metoda odpowiedzialna za powrót do głównego okna.
     */
     void on_return_button_clicked();
     /*!
     * \brief Metoda odpowiedzialna za jednorazowe nawodnienie.
+    *
+    * Metoda odpowiedzialna za jednorazowe nawodnienie - pozwala użytkownikowi natychmiast włączyć nawadnianie na określoną ilość czasu.
     */
     void on_radioButton_once_clicked();
     /*!
     * \brief Metoda odpowiedzialna za opublikowanie wiadomości.
+    *
+    * Metoda odpowiedzialna za zaakceptowanie wybranych przez użytkownika opcji i przesłanie ich do serwera.
     */
     void on_pushButton_accept_clicked();
     /*!
     * \brief Metoda odpowiedzialna za odświeżenie timera.
+    *
+    * Metoda odpowiedzialna za odświeżenie timera. Timer informuje o pozostałym czasie nawadniania
+    * (gdy zostało wybrane nawadnianie jednorazowe) oraz o pozostałym czasie schładzania wymaganym przez elektrozawór.
     */
     void updateCountdown();
     /*!
     * \brief Metoda odpowiedzialna wyłączenie nawadniania.
+    *
+    * Metoda odpowiedzialna wyłączenie nawadniania w wybranym dniu.
     */
     void on_pushButton_turn_off_clicked();
 
 signals:
     /*!
     * \brief Sygnał odpowiedzialny za zmianę okna.
+    *
+    * Sygnał odpowiedzialny za zmianę okna - ukrywa okno nawadniania i pokazuje okno ChooseWindow \ref ChooseWindow .
     */
     void change_window();
     /*!
     * \brief Sygnał odpowiedzialny za wysłanie wiadomości przez MQTT.
+    *
+    * Metoda wykorzystująca klienta MQTT do przesłania odpowiedniej wiadomości do odpowiedniego tematu.
     * \param[in] topic - temat do, którego publikujemy.
     * \param[in] msg - wiadomość jaką publikujemy.
     */
     void publish_msg(const QString &topic,const QByteArray &msg);
     /*!
     * \brief Sygnał odpowiedzialny za wysłanie wiadomości przez MQTT z opcją przetrzymania wiadomości.
+    *
+    * Metoda wykorzystująca klienta MQTT do przesłania odpowiedniej wiadomości do odpowiedniego tematu. Ponadto pozwala
+    * ustawić jakość serwisu oraz czy wiadomość powinna być przetrzymana przez serwer.
     * \param[in] topic - temat do, którego publikujemy.
     * \param[in] msg - wiadomość jaką publikujemy.
     * \param[in] qos - jakość serwisu (quality of service).
@@ -76,35 +99,51 @@ private:
     Ui::Watering *ui;
     /*!
     * \brief Liczba kropel, które chcemy animować.
+    *
+    * Liczba kropel, które chcemy animować.
     */
     static const int drop_count = 20;
     /*!
     * \brief Tablica etykiet do animacji kropel.
+    *
+    * Tablica etykiet do animacji kropel.
     */
     QLabel *labels[drop_count];
     /*!
     * \brief Tablica kropli.
+    *
+    * Tablica kropli.
     */
     Droplet *droplets[drop_count];
     /*!
     * \brief Tablica tematów do których jest wysyłane są wiadomości.
+    *
+    * Tablica tematów do których jest wysyłane są wiadomości związane z nawadnianiem.
     */
     const QString topics_watering[8] = { "garden/watering/solenoid/sunday", "garden/watering/solenoid/monday", "garden/watering/solenoid/tuesday", "garden/watering/solenoid/wednesday"
                                 , "garden/watering/solenoid/thursday", "garden/watering/solenoid/friday", "garden/watering/solenoid/saturday", "garden/watering/solenoid" };
     /*!
-    * \brief Timer odświeżający minutnik.
+    * \brief Timer odświeżający wyświetlany minutnik.
+    *
+    * Timer odświeżający wyświetlany minutnik informujący o pozostałym czasie nawadniania/schładznia.
     */
     QTimer *timer;
     /*!
     * \brief Timer do odświeżający minutnik ochładzania.
+    *
+    * Timer do odświeżający minutnik ochładzania.
     */
-    QTimer *timer2; // Sa dwa poniewaz musze wiedziec, ktory wyslal informacje (slot itd)
+    QTimer *timer2; //NOTE Sa dwa poniewaz musze wiedziec, ktory wyslal informacje (slot itd)
     /*!
-    * \brief Minutnik do wyświetlania pozostałego czasu nawadniania/ochładzania.
+    * \brief Minutnik do wyświetlania pozostałego czasu nawadniania/schładzania.
+    *
+    * Minutnik do wyświetlania pozostałego czasu nawadniania/schładzania.
     */
     QTime countdown;
     /*!
     * \brief Czas schładzania elektrozaworu.
+    *
+    * Czas schładzania elektrozaworu.
     */
     const unsigned short cooldown_time = 15;
 
--- weather.h
+++ weather.h
@@ -23,6 +23,8 @@ class Weather : public QDialog
 public:
     /*!
     * \brief Konstruktor klasy Weather.
+    *
+    * Konstruktor klasy Weather.
     * \param[in] parent
     */
     explicit Weather(QWidget *parent = nullptr);
@@ -30,20 +32,28 @@ public:
 public slots:
     /*!
     * \brief Metoda pobierająca dane pogodowe z serwera.
+    *
+    * Metoda pobierająca dane pogodowe z serwera wywoływana przy uruchomieniu programu, zmianie miejscowości oraz kliknięciu przycisku odświeżania.
     */
     void data_changed();
 private slots:
     /*!
     * \brief Metoda przewarzająca otrzymane informacje.
+    *
+    * Metoda umieszczająca otrzymane dane z serwera w odpowiednich kontenerach.
     * \param[in] reply - wskaźnik na odpowiedź serwera.
     */
     void managerFinished(QNetworkReply *reply);
     /*!
     * \brief Metoda powodująca powrót do głównego okna.
+    *
+    * Metoda odpowiedzialna za powrót do głównego okna.
     */
     void on_return_button_clicked();
     /*!
     * \brief Metoda odświeżająca pogodę.
+    *
+    * Metoda odświeżająca pogodę poprzez wysłanie żądania do serwera o nowe dane.
     */
     void on_pushButton_refresh_clicked();
 
@@ -51,104 +61,148 @@ private:
     Ui::Weather *ui;
     /*!
     * \brief Manager dostępu do sieci.
+    *
+    * Manager dostępu do sieci.
     */
     QNetworkAccessManager *manager;
     /*!
     * \brief Żądanie potrzebne managerowi.
+    *
+    * Żądanie potrzebne managerowi.
     */
     QNetworkRequest request;
     /*!
     * \brief Tablica odczuwalnych temperatur.
+    *
+    * Tablica odczuwalnych temperatur.
     */
     QVector<double> v_feels_like;
     /*!
     * \brief Tablica opadów deszczu.
+    *
+    * Tablica opadów deszczu.
     */
     QVector<double> v_rain;
     /*!
     * \brief Tablica ciścień.
+    *
+    * Tablica ciścień.
     */
     QVector<int> v_pressure;
     /*!
     * \brief Tablica temperatur.
+    *
+    * Tablica temperatur.
     */
     QVector<double> v_temp;
     /*!
-    * \brief Tablica opisów.
+    * \brief Tablica opisów pogody na dany dzień.
+    *
+    * Tablica opisów pogody na dany dzień.
     */
     QVector<QString> v_description;
     /*!
     * \brief Tablica ikon.
+    *
+    * Tablica ikon.
     */
     QVector<QString> v_icon;
     /*!
     * \brief Tablica dat.
+    *
+    * Tablica dat.
     */
     QVector<QString> v_time;
     /*!
-    * \brief Tablica chmur.
+    * \brief Tablica zachmurzeń.
+    *
+    * Tablica zachmurzeń.
     */
     QVector<int> v_clouds;
     /*!
     * \brief Metoda ustawiająca ikone na etykietę.
+    *
+    * Metoda ustawiająca ikone na etykietę.
     * \param[in] label - etykieta.
     * \param[in] icon - nazwa ikony.
     */
     void set_icon(QLabel *label, QString icon);
     /*!
     * \brief Metoda ustawiająca temperature na etykietę.
+    *
+    * Metoda ustawiająca temperature na etykietę.
     * \param[in] label - etykieta.
     * \param[in] temp - temperatura.
     */
     void set_temp(QLabel *label, double temp);
     /*!
     * \brief Metoda ustawiająca temperature odczuwalną na etykietę.
+    *
+    * Metoda ustawiająca temperature odczuwalną na etykietę.
     * \param[in] label - etykieta.
     * \param[in] feels_like - temperatura odczuwalna.
     */
     void set_feels(QLabel *label, double feels_like);
     /*!
     * \brief Metoda ustawiająca opady na etykietę.
+    *
+    * Metoda ustawiająca opady na etykietę.
     * \param[in] label - etykieta.
     * \param[in] rain - wartość opadów.
     */
     void set_rain(QLabel *label, double rain);
     /*!
     * \brief Metoda ustawiająca zachmurzenie na etykietę.
+    *
+    * Metoda ustawiająca zachmurzenie na etykietę.
     * \param[in] label - etykieta.
     * \param[in] cloudines - wartość zachmurzenia.
     */
     void set_clouds(QLabel *label,int cloudines);
     /*!
-    * \brief Metoda ustawiająca zachmurzenie na etykietę.
+    * \brief Metoda ustawiająca wartość ciśnienia na etykietę.
+    *
+    * Metoda ustawiająca wartość ciśnienia na etykietę.
     * \param[in] label - etykieta.
     * \param[in] pressure - wartość ciśnienia.
     */
     void set_pressure(QLabel *label,int pressure);
     /*!
     * \brief Metoda ustawiająca opis pogody na etykietę.
+    *
+    * Metoda ustawiająca opis pogody na etykietę.
     * \param[in] label - etykieta.
     * \param[in] description - opis pogody.
     */
     void set_description(QLabel *label, QString description);
     /*!
     * \brief Metoda ustawiająca datę na etykietę.
+    *
+    * Metoda ustawiająca datę na etykietę.
     * \param[in] label - etykieta.
     * \param[in] time - data.
     */
     void set_time(QLabel *label, QString time);
     /*!
     * \brief Metoda ustawiająca opisy i ikony.
+    *
+    * Metoda ustawiająca opisy i ikony na wszystkie dni przy użyciu odpowiednich metod.
     */
     void set_all_icons();
 
 
 signals:
     /*!
-    * \brief Sygnał .
+    * \brief Sygnał informujący, że dane zostały zmienione.
+    *
+    * Sygnał informujący, że dane zostały zmienione.
     */
     void daneZmienione(QJsonDocument dane);
-
+    /*!
+     * \brief Sygnał informujący o tym, że należy zmienić okno.
+     *
+     * Sygnał informujący o tym, że należy zmienić okno.
+     */
     void change_window();
 };
 
