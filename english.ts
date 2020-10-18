0a42be2cc52057d893eac430ee73575318314eed
Mateusz Urbaniak <urban1998@wp.pl>
2020-10-18 06:02:55 +0200

    English translation updated

--- english.ts
+++ english.ts
@@ -37,57 +37,68 @@ p, li { white-space: pre-wrap; }
     <name>Chickencoop</name>
     <message>
         <location filename="chickencoop.ui" line="14"/>
-        <location filename="chickencoop.cpp" line="86"/>
         <source>Kurnik</source>
         <translation>Chcickencoop</translation>
     </message>
     <message>
-        <location filename="chickencoop.ui" line="117"/>
         <source>Format</source>
-        <translation>Format</translation>
+        <translation type="vanished">Format</translation>
     </message>
     <message>
-        <location filename="chickencoop.ui" line="143"/>
         <source>Adres ip:</source>
-        <translation>IP address:</translation>
+        <translation type="vanished">IP address:</translation>
     </message>
     <message>
-        <location filename="chickencoop.ui" line="192"/>
         <source>h264</source>
-        <translation>h264</translation>
+        <translation type="vanished">h264</translation>
     </message>
     <message>
-        <location filename="chickencoop.ui" line="197"/>
         <source>mjpeg</source>
-        <translation>mjpeg</translation>
+        <translation type="vanished">mjpeg</translation>
     </message>
     <message>
-        <location filename="chickencoop.ui" line="94"/>
         <source>Port:</source>
-        <translation>Port:</translation>
+        <translation type="vanished">Port:</translation>
     </message>
     <message>
-        <location filename="chickencoop.ui" line="159"/>
         <source>Zmień stream</source>
-        <translation>Change stream</translation>
+        <translation type="vanished">Change stream</translation>
     </message>
     <message>
-        <location filename="chickencoop.cpp" line="91"/>
         <source>Wszystkie pola muszą być uzupełnione.</source>
-        <translation>Fill all boxes.</translation>
+        <translation type="vanished">Fill all boxes.</translation>
     </message>
     <message>
-        <location filename="chickencoop.cpp" line="93"/>
         <source>Zmiany zostaną wprowadzone po restarcie aplikacji.</source>
-        <translation>Restrt application to update changes.</translation>
+        <translation type="vanished">Restrt application to update changes.</translation>
+    </message>
+    <message>
+        <location filename="chickencoop.cpp" line="26"/>
+        <source>Otwórz drzwi kurnika</source>
+        <translation>Open chickencoop</translation>
+    </message>
+    <message>
+        <location filename="chickencoop.cpp" line="27"/>
+        <source>Zamknij drzwi kurnika</source>
+        <translation>Close chickencoop</translation>
+    </message>
+    <message>
+        <location filename="chickencoop.cpp" line="28"/>
+        <source>Powrót do okna głównego</source>
+        <translation>Return to main window</translation>
+    </message>
+    <message>
+        <location filename="chickencoop.cpp" line="29"/>
+        <source>Okno wyświetlania streamu</source>
+        <translation>Stream window</translation>
     </message>
 </context>
 <context>
     <name>ChooseWindow</name>
     <message>
         <location filename="choosewindow.ui" line="14"/>
-        <location filename="choosewindow.cpp" line="126"/>
         <location filename="choosewindow.cpp" line="137"/>
+        <location filename="choosewindow.cpp" line="148"/>
         <source>Inteligentny dom</source>
         <translation>Smart Home</translation>
     </message>
@@ -102,54 +113,83 @@ p, li { white-space: pre-wrap; }
         <translation>Options</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="497"/>
+        <location filename="choosewindow.ui" line="498"/>
         <source>Pomoc</source>
         <translation>Help</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="503"/>
+        <location filename="choosewindow.ui" line="504"/>
         <source>Język</source>
         <translation>Language</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="515"/>
-        <location filename="choosewindow.ui" line="520"/>
+        <location filename="choosewindow.ui" line="516"/>
+        <location filename="choosewindow.ui" line="521"/>
         <source>Ustawienia MQTT</source>
         <translation>MQTT settings</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="525"/>
+        <location filename="choosewindow.ui" line="526"/>
         <source>O autorze</source>
         <translation>About author</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="530"/>
+        <location filename="choosewindow.ui" line="531"/>
         <source>Ustawienia nawadniania</source>
         <translation>Watering settings</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="535"/>
+        <location filename="choosewindow.ui" line="536"/>
         <source>Ustawienia pogody</source>
         <translation>Weather settings</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="540"/>
+        <location filename="choosewindow.ui" line="541"/>
         <source>Polski</source>
         <translation>Polish</translation>
     </message>
     <message>
-        <location filename="choosewindow.ui" line="545"/>
+        <location filename="choosewindow.ui" line="546"/>
         <source>Angielski</source>
         <translation>English</translation>
     </message>
     <message>
-        <location filename="choosewindow.cpp" line="33"/>
-        <source>Temperatura, wilgotność i status otwarcia drzwi w kurniku.</source>
-        <translation>Temperature, humidity and door status in chickencoop.</translation>
+        <location filename="choosewindow.ui" line="551"/>
+        <source>Ustawienia streamu</source>
+        <translation>Stream settings</translation>
     </message>
     <message>
-        <location filename="choosewindow.cpp" line="127"/>
+        <source>Temperatura, wilgotność i status otwarcia drzwi w kurniku.</source>
+        <translation type="vanished">Temperature, humidity and door status in chickencoop.</translation>
+    </message>
+    <message>
+        <location filename="choosewindow.cpp" line="35"/>
+        <source>Temperatura, wilgotność i status otwarcia drzwi w kurniku</source>
+        <translation>Temperature, humidity and door status in chickencoop</translation>
+    </message>
+    <message>
+        <location filename="choosewindow.cpp" line="36"/>
+        <source>Okno sterowania kurnikiem</source>
+        <translation>Chickencoop window</translation>
+    </message>
+    <message>
+        <location filename="choosewindow.cpp" line="37"/>
+        <source>Okno sterowania nawodnieniem</source>
+        <translation>Watering control window</translation>
+    </message>
+    <message>
+        <location filename="choosewindow.cpp" line="38"/>
+        <source>Okno pogody</source>
+        <translation>Weather window</translation>
+    </message>
+    <message>
+        <location filename="choosewindow.cpp" line="39"/>
+        <source>Okno sterowania oświetleniem</source>
+        <translation>Lights control window</translation>
+    </message>
+    <message>
         <location filename="choosewindow.cpp" line="138"/>
+        <location filename="choosewindow.cpp" line="149"/>
         <source>Zmiany zostaną wprowadzone po restarcie aplikacji.</source>
         <translation>Restart application to update changes.</translation>
     </message>
@@ -167,30 +207,87 @@ p, li { white-space: pre-wrap; }
         <translation>IP address:</translation>
     </message>
     <message>
-        <location filename="settingsmqtt.ui" line="120"/>
+        <location filename="settingsmqtt.ui" line="125"/>
         <source>Port:</source>
         <translation>Port:</translation>
     </message>
     <message>
-        <location filename="settingsmqtt.ui" line="181"/>
+        <location filename="settingsmqtt.ui" line="191"/>
         <source>Login:</source>
         <translation>Login:</translation>
     </message>
     <message>
-        <location filename="settingsmqtt.ui" line="245"/>
+        <location filename="settingsmqtt.ui" line="260"/>
         <source>Hasło:</source>
         <translation>Password:</translation>
     </message>
     <message>
-        <location filename="settingsmqtt.ui" line="286"/>
+        <location filename="settingsmqtt.ui" line="311"/>
+        <source>Zapisz</source>
+        <translation>Save</translation>
+    </message>
+    <message>
         <source>Zapisz i wyjdź</source>
-        <translation>Save and close window</translation>
+        <translation type="vanished">Save and close window</translation>
     </message>
     <message>
         <location filename="settingsmqtt.cpp" line="26"/>
         <location filename="settingsmqtt.cpp" line="28"/>
         <source>Opcjonalnie</source>
         <translation>Optional</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsStream</name>
+    <message>
+        <location filename="settingsstream.ui" line="14"/>
+        <source>Ustawienia streamu</source>
+        <translation>Stream settings</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.ui" line="65"/>
+        <source>Adres ip:</source>
+        <translation>IP address:</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.ui" line="146"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.ui" line="227"/>
+        <source>Format</source>
+        <translation>Format:</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.ui" line="255"/>
+        <source>h264</source>
+        <translation>h264</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.ui" line="260"/>
+        <source>mjpeg</source>
+        <translation>mjpeg</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.ui" line="297"/>
+        <source>Zapisz</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.cpp" line="38"/>
+        <source>Kurnik</source>
+        <translation>Chcickencoop</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.cpp" line="43"/>
+        <source>Wszystkie pola muszą być uzupełnione.</source>
+        <translation>Fill all boxes.</translation>
+    </message>
+    <message>
+        <location filename="settingsstream.cpp" line="45"/>
+        <source>Zmiany zostaną wprowadzone po restarcie aplikacji.</source>
+        <translation>Settings will be saved after application restart.</translation>
     </message>
 </context>
 <context>
@@ -206,26 +303,30 @@ p, li { white-space: pre-wrap; }
         <translation>Latitude:</translation>
     </message>
     <message>
-        <location filename="settingsweather.ui" line="120"/>
+        <location filename="settingsweather.ui" line="125"/>
         <source>Długość geograficzna:</source>
         <translation>Longitude:</translation>
     </message>
     <message>
-        <location filename="settingsweather.ui" line="181"/>
+        <location filename="settingsweather.ui" line="191"/>
         <source>Klucz OpenWeather API:</source>
         <translation>OpenWeather API key:</translation>
     </message>
     <message>
-        <location filename="settingsweather.ui" line="222"/>
+        <location filename="settingsweather.ui" line="242"/>
+        <source>Zapisz</source>
+        <translation>Save</translation>
+    </message>
+    <message>
         <source>Zapisz i wyjdź</source>
-        <translation>Save and close window</translation>
+        <translation type="vanished">Save and close window</translation>
     </message>
 </context>
 <context>
     <name>Watering</name>
     <message>
         <location filename="watering.ui" line="19"/>
-        <location filename="watering.cpp" line="147"/>
+        <location filename="watering.cpp" line="150"/>
         <source>Nawadnianie</source>
         <translation>Watering</translation>
     </message>
@@ -276,8 +377,8 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <location filename="watering.ui" line="459"/>
-        <location filename="watering.cpp" line="122"/>
-        <location filename="watering.cpp" line="138"/>
+        <location filename="watering.cpp" line="125"/>
+        <location filename="watering.cpp" line="141"/>
         <source>na</source>
         <translation>for</translation>
     </message>
@@ -307,51 +408,56 @@ p, li { white-space: pre-wrap; }
         <translation>-- --</translation>
     </message>
     <message>
-        <location filename="watering.cpp" line="58"/>
-        <location filename="watering.cpp" line="64"/>
-        <location filename="watering.cpp" line="70"/>
-        <location filename="watering.cpp" line="76"/>
-        <location filename="watering.cpp" line="82"/>
-        <location filename="watering.cpp" line="88"/>
-        <location filename="watering.cpp" line="94"/>
+        <location filename="watering.cpp" line="35"/>
+        <source>Powrót do okna głównego</source>
+        <translation>Return to main window</translation>
+    </message>
+    <message>
+        <location filename="watering.cpp" line="61"/>
+        <location filename="watering.cpp" line="67"/>
+        <location filename="watering.cpp" line="73"/>
+        <location filename="watering.cpp" line="79"/>
+        <location filename="watering.cpp" line="85"/>
+        <location filename="watering.cpp" line="91"/>
+        <location filename="watering.cpp" line="97"/>
         <source>Włącz o </source>
         <translation>Turn on at </translation>
     </message>
     <message>
-        <location filename="watering.cpp" line="58"/>
-        <location filename="watering.cpp" line="64"/>
-        <location filename="watering.cpp" line="70"/>
-        <location filename="watering.cpp" line="76"/>
-        <location filename="watering.cpp" line="82"/>
-        <location filename="watering.cpp" line="88"/>
-        <location filename="watering.cpp" line="94"/>
+        <location filename="watering.cpp" line="61"/>
+        <location filename="watering.cpp" line="67"/>
+        <location filename="watering.cpp" line="73"/>
+        <location filename="watering.cpp" line="79"/>
+        <location filename="watering.cpp" line="85"/>
+        <location filename="watering.cpp" line="91"/>
+        <location filename="watering.cpp" line="97"/>
         <source>
 na </source>
         <translation>
 for </translation>
     </message>
     <message>
-        <location filename="watering.cpp" line="121"/>
+        <location filename="watering.cpp" line="124"/>
         <source>Włącz nawodnienie</source>
         <translation>Turn on watering</translation>
     </message>
     <message>
-        <location filename="watering.cpp" line="137"/>
+        <location filename="watering.cpp" line="140"/>
         <source>Włącz nawodnienie o</source>
         <translation>Turn on watering at</translation>
     </message>
     <message>
-        <location filename="watering.cpp" line="148"/>
+        <location filename="watering.cpp" line="151"/>
         <source>Musisz zaznaczyc regularność nawadniania.</source>
         <translation>Select watering days.</translation>
     </message>
     <message>
-        <location filename="watering.cpp" line="205"/>
+        <location filename="watering.cpp" line="208"/>
         <source>Pozostały czas nawadniania:</source>
         <translation>Remaing time:</translation>
     </message>
     <message>
-        <location filename="watering.cpp" line="223"/>
+        <location filename="watering.cpp" line="226"/>
         <source>Pozostały czas schładzania:</source>
         <translation>Cooldown time:</translation>
     </message>
@@ -414,37 +520,47 @@ for </translation>
         <translation type="vanished">Error</translation>
     </message>
     <message>
-        <location filename="weather.cpp" line="136"/>
+        <location filename="weather.cpp" line="25"/>
+        <source>Powrót do okna głównego</source>
+        <translation>Return to main window</translation>
+    </message>
+    <message>
+        <location filename="weather.cpp" line="26"/>
+        <source>Odśwież dane</source>
+        <translation>Refresh</translation>
+    </message>
+    <message>
+        <location filename="weather.cpp" line="137"/>
         <source>Temp.: </source>
         <translation>Temp.: </translation>
     </message>
     <message>
-        <location filename="weather.cpp" line="141"/>
+        <location filename="weather.cpp" line="142"/>
         <source>Odcz.: </source>
         <translation>Feels: </translation>
     </message>
     <message>
-        <location filename="weather.cpp" line="146"/>
+        <location filename="weather.cpp" line="147"/>
         <source>Opady: </source>
         <translation>Rain: </translation>
     </message>
     <message>
-        <location filename="weather.cpp" line="151"/>
+        <location filename="weather.cpp" line="152"/>
         <source>Chmury: </source>
         <translation>Clouds: </translation>
     </message>
     <message>
-        <location filename="weather.cpp" line="156"/>
+        <location filename="weather.cpp" line="157"/>
         <source>Ciśnienie: </source>
         <translation>Preasure: </translation>
     </message>
     <message>
-        <location filename="weather.cpp" line="235"/>
+        <location filename="weather.cpp" line="236"/>
         <source>Dzisiaj</source>
         <translation>Today</translation>
     </message>
     <message>
-        <location filename="weather.cpp" line="236"/>
+        <location filename="weather.cpp" line="237"/>
         <source>Jutro</source>
         <translation>Tomorrow</translation>
     </message>
